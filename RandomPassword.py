97408829a0ad5f8f6b570de3bd39e9f3f91129d0
JackBidwell <jackbidwell2002@gmail.com>
2024-06-15 14:10:48 -0500

    Loop to determine the position of every letter in a password in an alphabet

--- RandomPassword.py
+++ RandomPassword.py
@@ -2,18 +2,29 @@ import random
 import string 
 
 password = input("Enter your password: ")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabetposition = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
 
-i = 0
-while i <= 10:
-  password += (random.choice(string.digits))
-  i += 1
-
-print(password)
+# Determing a letters position in the alphabet
 
 i = 0
 while i < len(password):
-  if password[i] == "s":
-    print("This charecter is an s")
-  else:
-    print("This charecter is not an s")
+  j = 0
+  while j < len(alphabet):
+    if password[i] == alphabet[j]:
+      print(alphabetposition[j])
+    j += 1
   i += 1
+
+
+# print(password)
+
+# i = 0
+# while i < len(password):
+#   if password[i] < (alphabetposition)/2:
+#     password[i] = random.choice(string.digits)
+#   elif password[i] > (alphabetposition)/2:
+#     password[i] = alphabetposition - i
+#   i += 1 
+
+# print(password)
